f2de14e0d412154d461d182f4c94d84793da969d
Miguel Ângelo Nobre e Souza <miguel@ca-13-22.local>
2023-10-24 17:18:55 -0300

    static root

--- digproj/settings.py
+++ digproj/settings.py
@@ -131,7 +131,7 @@ USE_L10N = True
 USE_TZ = False #True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 AUTH_USER_MODEL = 'core.User'
 
